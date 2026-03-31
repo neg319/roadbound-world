06be2af03a8082662ff9588cac8a24e6cb99d504
neg319 <neg319@gmail.com>
2026-03-30 20:00:13 -0400

    Update PersistentHostileGroup.cs

--- Source/World/PersistentHostileGroup.cs
+++ Source/World/PersistentHostileGroup.cs
@@ -1,21 +1,20 @@
 using System.Collections.Generic;
-using RimWorld;
 using Verse;
 
 namespace RoadboundWorld.World;
 
 public sealed class PersistentHostileGroup : IExposable
 {
-    public int tile = -1;
-    public Direction8Way entryDirection = Direction8Way.North;
+    public int tile;
+    public int entryDirection;
     public int expiresAtTick;
     public List<PersistentHostileRecord> pawns = new();
 
     public void ExposeData()
     {
-        Scribe_Values.Look(ref tile, nameof(tile), -1);
-        Scribe_Values.Look(ref entryDirection, nameof(entryDirection), Direction8Way.North);
-        Scribe_Values.Look(ref expiresAtTick, nameof(expiresAtTick), 0);
+        Scribe_Values.Look(ref tile, nameof(tile));
+        Scribe_Values.Look(ref entryDirection, nameof(entryDirection));
+        Scribe_Values.Look(ref expiresAtTick, nameof(expiresAtTick));
         Scribe_Collections.Look(ref pawns, nameof(pawns), LookMode.Deep);
     }
 }
